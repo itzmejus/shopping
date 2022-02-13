1a7acfa3c79011641f439162a1a42043d85151cc
itzmejus <itzjustin3344@gmail.com>
2022-02-13 19:45:43 +0530

    login section edited

--- src/Routes/Account.js
+++ src/Routes/Account.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import '../Styles/Account.css'
+import user from '../media/user.png'
 function Account() {
   return (
-    <div>
-        <h1>This is Account page</h1>
+    <div className='account'>
+        <div className='profile'>
+       <div className='profile-left'>
+        <h1>Christophor</h1>
+        <img src={user} alt='profile'/>
+       </div>
+       <div className='profile-right'>
+         <h1>Details</h1>
+       </div>
+        
+        </div>
     </div>
   )
 }
--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import '../Styles/Login.css'
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from "react";
+import "../Styles/Login.css";
+
+import { useNavigate } from "react-router-dom";
 function Login() {
-    let navigate=useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  let navigate = useNavigate();
+
+  const log=(e)=>{
+    e.preventDefault();
+    console.log('logged success');
+  }
   return (
-   <div className='login'> 
-        <div className='logincard'>
-        <h1>Login</h1>
-        <input type='email' placeholder='please enter email'/>
-        <input type='password' placeholder='please enter password'/>
-        <button onClick={()=>{
-            navigate('/')
-            alert('Login success');
-        }
-       }>Login</button>
-        </div>
-   </div>
-  )
+    <div className="login">
+      <div className="logincard">
+        <h1 >Login</h1>
+        
+        <input type="email" placeholder="please enter email" onChange={(e)=>{
+          setEmail(e.target.value)
+        }} value={email}  />
+        <input type="password" placeholder="please enter password"  onChange={(e)=>{
+          setPassword(e.target.value)
+        }} value={password}/>
+        <button type="submit"
+          onClick={() => {
+            navigate("/");
+            alert("Login success");
+          }}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
